0e4604b583060f79414e2a79e7ddbbcd3a4c31d5
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2022-06-18 19:38:46 +0100

    folder structure

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { ConfigService } from '../config/config.service';
+import { ConfigService } from '../../config/config.service';
 import { jwtPayload } from './types';
 
 @Injectable()
