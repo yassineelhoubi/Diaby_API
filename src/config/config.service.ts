38cdde62d4124d7b87473f1b7ca2301b8c32a8e8
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2022-06-19 00:49:59 +0100

    debugging

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -12,7 +12,7 @@ export class ConfigService {
     private readonly envConfig: EnvConfig;
 
     constructor() {
-        const file: Buffer | undefined = fs.readFileSync('.env');
+        const file: Buffer | undefined = fs.readFileSync(process.env.NODE_ENV === 'production' ? '.env.prod' : '.env');
         const config = dotenv.parse(file);
         this.envConfig = this.validateInput(config);
     }
