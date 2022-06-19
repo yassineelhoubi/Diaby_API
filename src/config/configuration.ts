b0bd4cd683e74f302dbf6a6f48e67d804827aebf
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2022-06-19 18:08:39 +0100

    new configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,5 @@
 export default () => ({
-    port: parseInt(process.env.PORT, 10) || 3000,
+    port: process.env.PORT || 3000,
     DATABASE_URI: process.env.DATABASE_URI,
     USER_SECRET_KEY: process.env.USER_SECRET_KEY,
   });
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
-import { ConfigService } from '../../config/config.service';
+// import { ConfigService } from '../../config/config.service';
 import { JwtPayload } from './types';
 
 @Injectable()
