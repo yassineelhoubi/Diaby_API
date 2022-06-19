2bbeaa8563e9dca0b17957b24a7200e16235794e
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2022-06-19 18:13:35 +0100

    change importaion root

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '../../config/config.service';
+// import { ConfigService } from '../../config/config.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
